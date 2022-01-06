bfd6e1d577f6110bda0defad2ad72ef97efa7ac7
henrimacedo <henri.fcp1999@gmail.com>
2022-01-06 17:08:15 +0100

    __init__ modified

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="appromath",
-    version="1.0.2",
+    version="1.0.3",
     author="Ibrahima BAH, Kévin HENTZ, Alexandre RAMDOO, Henri MACEDO GONCALVES",
     author_email="henri.macedo-goncalves@etu.unistra.fr",
     description="Projet d'analyse: Sujet 31",
