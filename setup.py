ba8b89cd9b78ba40a08374416bba90d495da1c23
henrimacedo <henri.fcp1999@gmail.com>
2022-01-07 14:32:35 +0100

    README.md, setup.py, __init__.py modified

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="appromath",
-    version="1.0.3",
+    version="1.1.0",
     author="Ibrahima BAH, Kévin HENTZ, Alexandre RAMDOO, Henri MACEDO GONCALVES",
     author_email="henri.macedo-goncalves@etu.unistra.fr",
     description="Projet d'analyse: Sujet 31",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=find_packages(),
     url="https://github.com/henrimacedo/appromath.git",
     project_urls={
         "Bug Tracker": "https://github.com/henrimacedo/appromath.git/issues",
@@ -20,7 +21,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "appromath"},
-    packages=setuptools.find_packages(where="appromath"),
     python_requires=">=3.0",
 )
